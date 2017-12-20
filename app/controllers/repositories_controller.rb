48efeb16a31c818b0f0dd02da2a79ad92f2508cb
Scott Atkins <satkins16@gmail.com>
2017-12-20 13:12:19 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    Faraday.get 'https://api.github.com/search/repositories' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = 'Iv1.86941974cba12a69'
       req.params['client_secret'] = 'e2406af2832741e7c8430ba4922f955f70389e55'
     end
